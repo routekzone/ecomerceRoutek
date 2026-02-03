08726aafe57cef800f2a5c3866aea62e94323759
williansang <williansang.dev@gmail.com>
2026-02-03 01:29:12 -0500

    pagina de carito de compras

--- app/pages/shop/index.vue
+++ app/pages/shop/index.vue
@@ -9,16 +9,16 @@
 </script>
 
 <template>
-  <div class="min-h-screen dark:bg-[#050505] light:bg-zinc-50 text-white light:text-zinc-900 font-sans p-4 pb-79 relative transition-colors duration-500">
+  <div class="min-h-screen dark:bg-[#050505] light:bg-white text-white light:text-zinc-900 font-sans p-4 pb-79 relative transition-colors duration-500">
 
-    <header class="flex items-center justify-between mb-8 px-2">
-      <UButton icon="i-heroicons-chevron-left" variant="ghost" color="neutral" @click="$router.back()" class="light:text-zinc-900" />
-      <h1 class="text-lg font-bold tracking-tight uppercase italic text-white light:text-zinc-900">Carrito de Compras</h1>
-      <div class="w-10"></div>
+    <header class="flex items-center justify-between mb-6 px-2">
+      <UButton icon="i-heroicons-chevron-left"  variant="none"  @click="$router.back()" class="light:text-white bg-black/80 rounded-full px-2 py-2 hover:scale-110 transition-all duration-500" />
+      <h1 class="text-3xl font-black tracking-wider  uppercase text-white light:text-zinc-900"> bolsa </h1>
+      <div class="w-5"></div>  
     </header>
 
     <div 
-      class="relative z-20 bg-gray-400/10 dark:bg-gray-400/10 light:bg-zinc-200/50 px-1 pb-8 pt-2 rounded-3xl rounded-b-none min-h-[40px] transition-all duration-500"
+      class="relative z-20 bg-gray-400/10 dark:bg-gray-400/10 light:bg-gray-700/15 px-1 pb-8 pt-2 rounded-3xl rounded-b-none min-h-[40px] transition-all duration-500"
       :class="cartItems.length > 0 && cartItems.length < 2 ? 'min-h-[40vh]' : 'min-h-[40px]'"
     >
       <div v-for="item in cartItems" :key="item.id" 
@@ -31,28 +31,28 @@
 
         <div class="flex-1">
           <div class="flex justify-between">
-            <h3 class="font-bold text-sm uppercase tracking-tighter text-white light:text-zinc-900">{{ item.name }}</h3>
-            <span class="text-[10px] text-zinc-500 uppercase">{{ item.brand || 'ROK' }}</span>
+            <h3 class="font-bold text-[14px] uppercase tracking-tighter text-white light:text-zinc-600">{{ item.name }}</h3>
+            <span class="text-[11px] font-black text-[#92bf32] uppercase">{{ item.brand || 'ROK' }}</span>
           </div>
-          <p class="text-sm font-black text-violet-400 mt-1 italic">S/ {{ item.price.toFixed(2) }}</p>
+          <p class="text-sm font-black text-indigo-500 mt-1">S/ {{ item.price.toFixed(2) }}</p>
 
           <div class="flex items-center justify-between mt-3">
-            <div class="flex items-center gap-4 bg-black/60 dark:bg-black/60 light:bg-zinc-100 rounded-full px-4 py-1.5 border border-zinc-800 dark:border-zinc-800 light:border-zinc-200 transition-colors">
-              <button class="text-zinc-500 active:scale-90 hover:text-violet-500" @click="cartStore.updateQty(item.id, -1)">
+            <!-- Boton subir y bajar S -->
+            <div class="flex items-center gap-4 bg-black/60 dark:bg-black/60 light:bg-yellow-500 rounded-full px-4 py-1 border border-zinc-800 dark:border-zinc-800 light:border-zinc-200 transition-colors">
+              <button class="text-zinc-800 mt-1 active:scale-90 hover:text-violet-500" @click="cartStore.updateQty(item.id, -1)">
                 <UIcon name="i-heroicons-minus" class="w-3.5 h-3.5" />
               </button>
-              <span class="text-xs font-bold w-4 text-center text-white light:text-zinc-900">{{ item.qty }}</span>
-              <button class="text-zinc-500 active:scale-90 hover:text-violet-500" @click="cartStore.updateQty(item.id, 1)">
-                <UIcon name="i-heroicons-plus" class="w-3.5 h-3.5" />
+              <span class="text-[15px] font-black w-4 text-center text-white light:text-white">{{ item.qty }}</span>
+              <button class="text-zinc-800 mt-1 active:scale-90 hover:text-violet-500" @click="cartStore.updateQty(item.id, 1)">
+                <UIcon name="i-heroicons-plus" class="w-4 h-4" />
               </button>
             </div>
             
             <UButton 
               icon="i-heroicons-trash" 
-              variant="ghost" 
-              color="neutral" 
-              size="xs"
-              class="text-zinc-600 hover:text-red-500 light:text-zinc-400"
+              variant="none" 
+              size="md"
+              class="text-white hover:bg-gray-200 hover:scale-120 active:bg-rose-500 transition-all duration-500 rounded-full  light:text-rose-400"
               @click="cartStore.removeFromCart(item.id)"
             />
           </div>
@@ -74,21 +74,21 @@
     <div class="fixed bottom-28 left-4 right-4 bg-zinc-900/95 dark:bg-zinc-900/95 light:bg-white backdrop-blur-md border border-zinc-800 dark:border-zinc-800 light:border-zinc-200 rounded-[32px] rounded-t-none p-6 shadow-2xl z-40 transition-all duration-500">
       <div class="space-y-3 mb-6">
         <div class="flex justify-between text-xs text-zinc-400 uppercase tracking-tighter">
-          <span>Subtotal</span>
-          <span class="text-white light:text-zinc-900 font-bold">S/ {{ subtotal.toFixed(2) }}</span>
+          <span class=" text-sky-700 text-[15px] font-bold"> Subtotal</span>
+          <span class="text-white light:text-black/60 text-[16px] font-bold">S/ {{ subtotal.toFixed(2) }}</span>
         </div>
         <div class="flex justify-between text-xs text-zinc-400 uppercase tracking-tighter">
-          <span>Envío Estimado</span>
-          <span class="text-white light:text-zinc-900 font-bold">S/ {{ shipping.toFixed(2) }}</span>
+          <span class=" text-rose-600 text-[13px] font-bold">Envío Estimado</span>
+          <span class="text-white light:text-black/60 font-bold text-[14px]">S/ {{ shipping.toFixed(2) }}</span>
         </div>
-        <div class="border-t border-zinc-800 dark:border-zinc-800 light:border-zinc-100 pt-4 flex justify-between items-baseline">
-          <span class="font-black text-sm uppercase italic text-white light:text-zinc-900">Total</span>
-          <span class="font-black text-2xl text-violet-500 italic">S/ {{ (subtotal + shipping).toFixed(2) }}</span>
+        <div class="border-t border-zinc-800 dark:border-zinc-800 light:border-zinc-300 pt-4 flex justify-between items-baseline">
+          <span class="font-black text-[18px] uppercase text-white light:text-zinc-900">Total</span>
+          <span class="font-black text-2xl text-indigo-500">S/ {{ (subtotal + shipping).toFixed(2) }}</span>
         </div>
       </div>
 
-      <UButton label="Continuar al pago" size="xl" block
-        class="rounded-2xl bg-[#00e676] text-black font-black hover:bg-[#00c853] transition-all py-4 uppercase text-xs tracking-widest shadow-[0_0_20px_rgba(0,230,118,0.3)]" />
+      <UButton label="Continuar al pago" size="xl" block variant="none"
+        class="rounded-2xl bg-black text-white font-black hover:scale-105 active:bg-linear-to-r from-green-700 to-indigo-800 transition-all duration-500 py-4 uppercase text-xs tracking-widest shadow-[0_0_20px_rgba(0,230,118,0.3)]" />
     </div>
   </div>
 </template>
